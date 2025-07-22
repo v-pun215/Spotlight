e6ac339b5bdfd8e0bd8ca2ecd015d1e63aa1cf85
v-pun215 <vihaan.pundir26@gmail.com>
2025-07-22 18:51:56 +0530

    change port for deployment

--- api/api.js
+++ api/api.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Allow frontend (5500) to access backend
 app.use(cors({
